23f20463b2cbc721b12692b2dce18225530e6684
Akiranya <porridgen@gmail.com>
2023-01-24 14:30:07 +0800

    Change listener priority & clean up code

--- Core/src/main/java/su/nightexpress/excellentenchants/hook/impl/ProtocolHook.java
+++ Core/src/main/java/su/nightexpress/excellentenchants/hook/impl/ProtocolHook.java
@@ -3,6 +3,7 @@ package su.nightexpress.excellentenchants.hook.impl;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
@@ -10,6 +11,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.GameMode;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -36,53 +38,51 @@ public class ProtocolHook {
 
         ProtocolManager manager = ProtocolLibrary.getProtocolManager();
 
-        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, PacketType.Play.Client.SET_CREATIVE_SLOT) {
+        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, ListenerPriority.LOWEST, PacketType.Play.Client.SET_CREATIVE_SLOT) {
             // modifying the items received from creative players
             @Override public void onPacketReceiving(final PacketEvent event) {
                 PacketContainer packet = event.getPacket();
 
                 ItemStack item = packet.getItemModifier().read(0);
-                ItemStack modified = revert(item);
-                packet.getItemModifier().write(0, modified);
+                ItemStack reverted = revert(item);
+                packet.getItemModifier().write(0, reverted);
             }
         });
 
-        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, PacketType.Play.Server.SET_SLOT) {
+        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, ListenerPriority.HIGHEST, PacketType.Play.Server.SET_SLOT) {
             // write lore when the server sets an item in a slot
             @Override public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
 
-                // Packet is buggy with creative mode, we just don't handle it
-                // if (event.getPlayer().getGameMode() == GameMode.CREATIVE) return;
-
+                boolean isCreative = event.getPlayer().getGameMode() == GameMode.CREATIVE;
                 ItemStack item = packet.getItemModifier().read(0);
-                packet.getItemModifier().write(0, update(item)); // TODO directly use modify
+                ItemStack updated = update(item, isCreative);
+                packet.getItemModifier().write(0, updated);
             }
         });
 
-        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, PacketType.Play.Server.WINDOW_ITEMS) {
+        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, ListenerPriority.HIGHEST, PacketType.Play.Server.WINDOW_ITEMS) {
             // write lore when the server sets a window of items
             @Override public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
 
-                // Packet is buggy with creative mode, we just don't handle it
-                // if (event.getPlayer().getGameMode() == GameMode.CREATIVE) return;
-
+                boolean isCreative = event.getPlayer().getGameMode() == GameMode.CREATIVE;
                 List<ItemStack> items = packet.getItemListModifier().readSafely(0);
-                items.replaceAll(ProtocolHook::update);
-                packet.getItemListModifier().write(0, items); // TODO directly use modify
+                items.replaceAll(item -> update(item, isCreative));
+                packet.getItemListModifier().write(0, items);
             }
         });
 
-        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, PacketType.Play.Server.OPEN_WINDOW_MERCHANT) {
+        manager.addPacketListener(new PacketAdapter(ExcellentEnchantsAPI.PLUGIN, ListenerPriority.HIGHEST, PacketType.Play.Server.OPEN_WINDOW_MERCHANT) {
             // write lore when the server sends the merchant recipes
             @Override
             public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
 
                 List<MerchantRecipe> list = new ArrayList<>();
+                boolean isCreative = event.getPlayer().getGameMode() == GameMode.CREATIVE;
                 packet.getMerchantRecipeLists().read(0).forEach(recipe -> {
-                    ItemStack result = update(recipe.getResult());
+                    ItemStack result = update(recipe.getResult(), isCreative);
                     if (result == null) return;
 
                     MerchantRecipe r2 = new MerchantRecipe(result, recipe.getUses(), recipe.getMaxUses(), recipe.hasExperienceReward(), recipe.getVillagerExperience(), recipe.getPriceMultiplier(), recipe.getDemand(), recipe.getSpecialPrice());
@@ -96,24 +96,7 @@ public class ProtocolHook {
         isRegistered = true;
     }
 
-    private static @Nullable ItemStack revert(@Nullable ItemStack item) {
-        if (item == null || item.getType().isAir()) return item;
-
-        if (!item.hasItemMeta()) return item; // if this is simple item
-        ItemMeta meta = item.getItemMeta();
-        if (meta == null || !meta.hasLore()) return item; // if no lore on this item
-
-        List<Component> lore = Objects.requireNonNull(meta.lore());
-        int size = EnchantManager.getExcellentEnchantments(item).size();
-        if (size == 0) return item; // if no custom enchantment on this item
-        List<Component> reverted = lore.subList(size, lore.size());
-        meta.lore(reverted.isEmpty() ? null : reverted); // remove the custom enchantment lore
-
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    private static @Nullable ItemStack update(@Nullable ItemStack item) {
+    private static @Nullable ItemStack update(@Nullable ItemStack item, boolean isCreative) {
         if (item == null || item.getType().isAir()) return item;
 
         if (!item.hasItemMeta()) return item; // if this is simple item
@@ -129,8 +112,8 @@ public class ProtocolHook {
         //     .sorted(Comparator.comparing(e -> e.getKey().getTier().getPriority()))
         //     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (old, nev) -> nev, LinkedHashMap::new));
 
-        // Add verbose enchantment description lore
-        if (Config.ENCHANTMENTS_DESCRIPTION_ENABLED.get()) {
+        // Add verbose enchantment description lore (except for creative mode)
+        if (Config.ENCHANTMENTS_DESCRIPTION_ENABLED.get() && !isCreative) {
             enchants.forEach((enchant, level) -> {
                 List<Component> desc = new ArrayList<>(ComponentUtil.asComponent(enchant.formatDescription(level)));
                 desc.replaceAll(component -> component.applyFallbackStyle(FALLBACK_STYLE));
@@ -138,13 +121,30 @@ public class ProtocolHook {
             });
         }
 
-        // Add basic enchantment lore (mimic vanilla one)
+        // Add vanilla-like enchantment lore
         enchants.forEach((enchant, level) -> {
             int charges = EnchantManager.getEnchantmentCharges(item, enchant);
             lore.add(0, enchant.displayName(level, charges));
         });
 
         meta.lore(lore);
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    private static @Nullable ItemStack revert(@Nullable ItemStack item) {
+        if (item == null || item.getType().isAir()) return item;
+
+        if (!item.hasItemMeta()) return item; // if this is simple item
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null || !meta.hasLore()) return item; // if no lore on this item
+
+        List<Component> lore = Objects.requireNonNull(meta.lore());
+        int size = EnchantManager.getExcellentEnchantments(item).size();
+        if (size == 0) return item; // if no custom enchantment on this item
+        List<Component> reverted = lore.subList(size, lore.size());
+        meta.lore(reverted.isEmpty() ? null : reverted); // remove the custom enchantment lore
+
         item.setItemMeta(meta);
         return item;
     }
